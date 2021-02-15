9443f6aa3cc415ddc4974a2638a3c5b44fa03d18
pacoyelo <fernando.lopez.pajares@gmail.com>
2021-02-15 09:43:44 +0100

    Funciones de primer nivel

--- suma_numeros.py
+++ suma_numeros.py
@@ -11,5 +11,13 @@ def mayusculas(palabra):
         print(letra.upper(), end='')
     print()
 
+def cuadrados(valores):
+    cuadrado = 0
+    for valor in range (0, valores+1):
+        cuadrado += valor**2
+    return cuadrado
+
+
 print(sumador(100))
 mayusculas('hola')
+print(cuadrados(3))
